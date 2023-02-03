880f0ce65883633ac1778c31ab63c93239806e00
Theresa Wiesner <109789713+twie44@users.noreply.github.com>
2023-02-03 09:40:21 +0100

    click event kitty

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var button = document.querySelector(".show-cat");
+// console.log(button);
+
+var cat = document.querySelector(".cat");
+// console.log(cat);
+
+button.addEventListener("click", function () {
+cat.classList.add("show")}
+);
