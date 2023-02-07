e41d866d702178bd96952cc21871e72c4173148b
Theresa Wiesner <109789713+twie44@users.noreply.github.com>
2023-02-07 08:34:41 +0100

    changed button text

--- js/script.js
+++ js/script.js
@@ -5,5 +5,13 @@ var cat = document.querySelector(".cat");
 // console.log(cat);
 
 button.addEventListener("click", function () {
-cat.classList.add("show")}
-);
+if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+} else { 
+    cat.classList.add("show");
+    button.innerText ="Shoo, cat!!";
+    button.classList.remove("disappear");
+    }
+});
